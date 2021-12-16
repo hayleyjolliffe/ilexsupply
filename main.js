2a0e416ec82e355ee69074fe69c93f0588572feb
hayley jolliffe <93291089+hayleyjolliffe@users.noreply.github.com>
2021-12-16 16:22:55 -0500

    Added arrays for timestamp function

--- main.js
+++ main.js
@@ -39,7 +39,23 @@ var aProducts = [];
 // Creating cart inventory array
 var aCart = [];
 
+// Creating timestamp function arrays
+var aDays = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."];
+var aMonths = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "Jun.", "Jul.", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
 // VARIABLES
 // Creating default currency variables
 var cValue = 1;
 var cSymbol = "$CAD";
+
+
+/*
+
+    FUNCTIONS
+
+*/
+// Creating TIMESTAMP function
+function timestamp()
+{
+    var current = new Date();
+}
